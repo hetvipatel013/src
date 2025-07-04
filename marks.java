e6491096635b5df8a94e9ca1f853c5ca55b0ca2e
123-varshini <varshinijothi17@gmail.com>
2025-07-04 09:22:57 +0530

    class location

---
+++ marks.java
@@ -0,0 +1,10 @@
+public class marks {
+    public static void main(String []args){
+        int [] marks = {90,97,98,96,78};
+        for ( int mark:marks){
+            System.out.println( mark);
+
+        }
+    }
+    
+}
